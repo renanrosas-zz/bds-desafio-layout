2cecfb1b33085b44607fd834614e2bbcff3c43ee
Renan Rosas <renan-rosas@hotmail.com>
2021-07-13 20:51:47 -0300

    Project created

---
+++ src/Routes.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact>
+            </Route>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
